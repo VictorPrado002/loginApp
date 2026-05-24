c8d19bf20d6c93ba937278ffb985f47c6f1954d0
VictorPrado002 <victor000hugo10@gmail.com>
2026-05-24 17:07:41 -0600

    create hook for auth

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,49 +1,20 @@
-import { useReducer } from "react";
 import { LoginPage } from "./auth/pages/LoginPage";
 import { UsersPage } from "./pages/UsersPage";
-import { loginReducer } from "./auth/reducers/loginReducer";
-import Swal from "sweetalert2";
 import { Navbar } from "./components/layout/Navbar";
-
-const initialLogin =JSON.parse(sessionStorage.getItem('login')) ||{
-  isAuth:false,
-  user:undefined
-}
+import { useAuth } from "./auth/hooks/useAuth";
 
 export const UsersApp = () => {
-  const [login,dispatch]=useReducer(loginReducer,initialLogin);
-  const handlerLogin =({username,password})=>{
-    if (username === "admin" && password === "12345") {
-      const user ={username:"admin"}
-
-      dispatch({
-        type:"login",
-        payload:user
-      });
-      sessionStorage.setItem('login',JSON.stringify({
-        isAuth:true,
-        user
-      }));
-
-    } else {
-      Swal.fire("Error Login!", "Username o password invalidos.", "error");
-    }
-  }
-
-  const handlerLogout =() =>{
-    dispatch({
-      type:'logout'
-    });
-    sessionStorage.removeItem('login');
-  }
-  return <> 
-  {
-    login.isAuth
-      ? <>
+  const { login, handlerLogin, handlerLogout } = useAuth();
+  return (
+    <>
+      {login.isAuth ? (
+        <>
           <Navbar login={login} handlerLogout={handlerLogout} />
-          <UsersPage/>
+          <UsersPage />
         </>
-      :<LoginPage handlerLogin={handlerLogin}/>
-  }
-  </>;
+      ) : (
+        <LoginPage handlerLogin={handlerLogin} />
+      )}
+    </>
+  );
 };
