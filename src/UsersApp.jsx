bccc46a7b05642b00a4066a4f622203b82243b93
VictorPrado002 <victor000hugo10@gmail.com>
2026-05-24 17:00:53 -0600

    logout implemented

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -3,11 +3,13 @@ import { LoginPage } from "./auth/pages/LoginPage";
 import { UsersPage } from "./pages/UsersPage";
 import { loginReducer } from "./auth/reducers/loginReducer";
 import Swal from "sweetalert2";
+import { Navbar } from "./components/layout/Navbar";
 
-const initialLogin ={
+const initialLogin =JSON.parse(sessionStorage.getItem('login')) ||{
   isAuth:false,
   user:undefined
 }
+
 export const UsersApp = () => {
   const [login,dispatch]=useReducer(loginReducer,initialLogin);
   const handlerLogin =({username,password})=>{
@@ -18,15 +20,30 @@ export const UsersApp = () => {
         type:"login",
         payload:user
       });
+      sessionStorage.setItem('login',JSON.stringify({
+        isAuth:true,
+        user
+      }));
 
     } else {
       Swal.fire("Error Login!", "Username o password invalidos.", "error");
     }
   }
+
+  const handlerLogout =() =>{
+    dispatch({
+      type:'logout'
+    });
+    sessionStorage.removeItem('login');
+  }
   return <> 
-  {login.isAuth
-  ? <UsersPage/>
-  :<LoginPage handlerLogin={handlerLogin}/>
+  {
+    login.isAuth
+      ? <>
+          <Navbar login={login} handlerLogout={handlerLogout} />
+          <UsersPage/>
+        </>
+      :<LoginPage handlerLogin={handlerLogin}/>
   }
   </>;
 };
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,32 @@
+export const Navbar = ({ login, handlerLogout }) => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand">UsersApp</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNavAltMarkup"
+        >
+          <span className="nav-item nav-link text-primary mx-3">
+            {login.user.username}
+          </span>
+          <button 
+          onClick={handlerLogout}className="btn btn-outline-success">
+            Logout
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
