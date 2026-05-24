4e64bc325312be80d32d5ef823049c3a4cc976bd
VictorPrado002 <victor000hugo10@gmail.com>
2026-05-24 02:40:26 -0600

    sweetalert implement

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,52 +1,19 @@
-import { useReducer, useState } from "react";
 import { UsersList } from "./components/UsersList";
 import { UserForm } from "./components/UserForm";
-import { usersReducer } from "./reducers/usersReducer";
+import { useUsers } from "./hooks/useUsers";
 
-const initialUsers = [
-  {
-    id: 1,
-    username: 'Victor',
-    password: '12345',
-    email: 'pepe@correo.com',
-  },
-];
 
-const initialUserForm={
-    id: 0,
-    username:'',
-    password:'',
-    email:'',
-}
 
 export const UsersApp = () =>{
-    const [users, dispatch] = useReducer(usersReducer, initialUsers);
-    const [userSelected,setUserSelected] =useState(initialUserForm);
     
-    const handlerAddUser = (user) => {
-        let type;
-
-        if (user.id ===0){
-          type='addUser';
-        }else{
-          type='updateUser'
-        }
-        dispatch({
-        type,
-        payload: user,
-        });
-    };
-    const handlerRemoveUser = (id) => {
-        console.log(id);
-        dispatch({
-        type: 'removeUser',
-        payload: id,
-        });
-    };
-    const handlerUserSelectedForm =(user) =>{
-      //console.log(user);
-      setUserSelected({...user});
-    }
+    const {
+        users,
+        userSelected,
+        initialUserForm,
+        handlerAddUser,
+        handlerRemoveUser,
+        handlerUserSelectedForm,
+    } = useUsers();
 
     return (
          <div className="container my-4 ">
