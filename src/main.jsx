e6c9375cdb2170fed010ed48dab69d8c54d6f697
VictorPrado002 <victor000hugo10@gmail.com>
2026-07-06 17:00:59 -0600

    feature: use auth context to handdle the data

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import { LoginPage } from "./auth/pages/LoginPage";
 import "./styles.css";
 import { UsersPage } from "./pages/UsersPage";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./auth/context/AuthProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <UsersApp />
+      <AuthProvider>
+        <UsersApp />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 );
